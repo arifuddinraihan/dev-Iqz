f7c17d78873aad8b905fc52faa05b08177b0f797
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-10 22:02:44 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar.js/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>This is my first Quiz Website</h1>
     </div>
   );
---
+++ src/Components/Navbar.js/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-slate-300 text-slate-700 sticky shadow-xl">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content md:content-none mt-3 p-2 shadow bg-slate-300 rounded-box w-52">
+                        <li><a>Homepage</a></li>
+                        <li><a>Stats</a></li>
+                        <li><a>Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center">
+                <a className="btn btn-ghost normal-case text-xl">Dev Iqz</a>
+            </div>
+            <div className="navbar-end">
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="https://placeimg.com/80/80/people" />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-300 rounded-box w-52">
+                        <li><a>User</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;

b3147dfd407a0ebb2a0801a66eae93f921236801
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-12 00:42:04 +0600

    show ansr eye added to questions

--- src/Components/Quiz/Questions.js
+++ src/Components/Quiz/Questions.js
@@ -1,9 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faEye } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 
 const Questions = ({ question }) => {
     const { question: ques, options, correctAnswer } = question;
+    const showCorrectAns = () => {
+        Swal.fire({
+            title : 'Correct Answer',
+            text : correctAnswer,
+            confirmButtonText: 'OK'
+        })
+    }
     const optionBtnHandle = (data) => {
         if (data === correctAnswer) {
             toast.success('Bravo! Correct Answer.', {
@@ -34,17 +42,21 @@ const Questions = ({ question }) => {
     return (
         <div className="card card-side bg-slate-300 shadow-xl">
             <div className="card-body">
-                <div className="card-title" >
-                    <h2 className="text-start text-black" dangerouslySetInnerHTML={{ __html: ques }}></h2>
-                    <FontAwesomeIcon></FontAwesomeIcon>
+                <div className="text-black font-bold text-xs md:text-lg flex items-center" >
+                    <h2 className="text-start text-black flex-grow" dangerouslySetInnerHTML={{ __html: ques }}></h2>
+                    <FontAwesomeIcon
+                        icon={faEye}
+                        className="text-slate-500 btn btn-ghost btn-xs"
+                        onClick={showCorrectAns}></FontAwesomeIcon>
                 </div>
                 <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
                     {
                         options.map((option, index) =>
                             <li key={index}
-                                className='btn btn-secondary flex flex-nowrap my-4 md:my-0 text-slate-800'
+                                className='btn btn-secondary flex flex-nowrap py-8 text-slate-800'
                                 onClick={() => optionBtnHandle(option)}>
-                                <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
+                                <FontAwesomeIcon 
+                                icon={faCircle}></FontAwesomeIcon>
                                 <p className='flex-grow text-start ml-2'>
                                     {option}
                                 </p>

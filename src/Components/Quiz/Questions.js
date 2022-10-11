e9fbf8c7a439ed7e6d48bdcef52f6248eb5acae9
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-11 22:22:45 +0600

    footer and quiz option error fixed

--- src/Components/Quiz/Questions.js
+++ src/Components/Quiz/Questions.js
@@ -4,8 +4,7 @@ import { toast } from 'react-toastify';
 
 const Questions = ({ question }) => {
     const { question: ques, options, correctAnswer } = question;
-
-    const optionBtnHandle = () => {
+    function optionBtnHandle() {
         options.find(rightAnswer => {
             if (rightAnswer !== correctAnswer) {
                 toast.success('Correct Answer!', {
@@ -19,21 +18,26 @@ const Questions = ({ question }) => {
                     theme: "dark",
                 });
             } else {
-                console.log("false Answer")
+                console.log("false Answer");
             }
-        })
+        });
     }
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title text-start">{ques}</h2>
-                <ul className="flex flex-col gap-2">
+                <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
                     {
-                        options.map(option => <li className='btn btn-ghost flex flex-nowrap my-4 md:my-0' onClick={optionBtnHandle}>
-                            <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
-                            <p className='flex-grow text-start ml-2'>{option}</p>
-                        </li>)
+                        options.map((option, index) =>
+                            <li key={index}
+                                className='btn btn-ghost flex flex-nowrap my-4 md:my-0'
+                                onClick={optionBtnHandle}>
+                                <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
+                                <p className='flex-grow text-start ml-2'>
+                                    {option}
+                                </p>
+                            </li>)
                     }
                 </ul>
             </div>

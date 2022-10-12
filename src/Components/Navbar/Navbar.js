00c1f8e513dd8d0930f0288f2ee94426af444e4d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-12 21:19:01 +0600

    added active link classname

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeMerge} from '@fortawesome/free-solid-svg-icons';
 
@@ -12,9 +12,9 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content md:content-none mt-3 p-2 shadow bg-slate-300 rounded-box w-52">
-                        <li><Link to={'/home'}>Homepage</Link></li>
-                        <li><Link to={'/stat'}>Stats</Link></li>
-                        <li><Link to={'/blog'}>Blog</Link></li>
+                        <li><NavLink to={'/home'} className={({isActive})=> isActive ? 'bg-cyan-400 font-bold' : undefined}>Homepage</NavLink></li>
+                        <li><NavLink to={'/stat'} className={({isActive})=> isActive ? 'bg-cyan-400 font-bold' : undefined}>Stats</NavLink></li>
+                        <li><NavLink to={'/blog'} className={({isActive})=> isActive ? 'bg-cyan-400 font-bold' : undefined}>Blog</NavLink></li>
                     </ul>
                 </div>
             </div>
